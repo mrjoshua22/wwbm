0b934c67ee1d99197dad1f99dcff332669eab297
mr_joshua <ulanov.ng@gmail.com>
2023-02-05 18:28:32 +0300

    Refactor requests specs

--- spec/requests/games_spec.rb
+++ spec/requests/games_spec.rb
@@ -4,152 +4,193 @@ RSpec::Matchers.define_negated_matcher :not_change, :change
 
 RSpec.describe "Games", type: :request do
   let(:user) { create(:user) }
-  let(:admin) { create(:user, is_admin: true) }
   let(:game_w_questions) { create(:game_with_questions, user: user) }
 
-  context 'Anonymous user' do
-    it 'kicks from #create' do
-      expect { post games_path }.not_to change(Game,:count)
+  context 'Unregistered user' do
+    describe '#create' do
+      before(:context) do
+        post games_path
+      end
 
-      expect(response.status).not_to eq(200)
-      expect(response).to redirect_to(new_user_session_path)
-      expect(flash[:alert]).to be
+      it { expect { post games_path }.not_to change(Game,:count) }
+      it { expect(response.status).not_to eq(200) }
+      it { expect(response).to redirect_to(new_user_session_path) }
+      it { expect(flash[:alert]).to be }
     end
 
-    it 'kicks from #show' do
-      get game_path(game_w_questions)
+    describe '#show' do
+      before(:example) do
+        get game_path(game_w_questions)
+      end
 
-      expect(response.status).not_to eq(200)
-      expect(response).to redirect_to(new_user_session_path)
-      expect(flash[:alert]).to be
+      it { expect(response.status).not_to eq(200) }
+      it { expect(response).to redirect_to(new_user_session_path) }
+      it { expect(flash[:alert]).to be }
     end
 
-    it 'kicks from #answer' do
-      expect { put answer_game_path(game_w_questions),
-        params: {
+    describe '#answer' do
+      before(:example) do
+        put answer_game_path(game_w_questions), params: {
           letter: game_w_questions.current_game_question.correct_answer_key
-        }}.not_to change { game_w_questions.current_level }
+        }
+      end
 
-      expect(response.status).not_to eq(200)
-      expect(response).to redirect_to(new_user_session_path)
-      expect(flash[:alert]).to be
+      it 'does not change game level' do
+        expect { put answer_game_path(game_w_questions), params: {
+          letter: game_w_questions.current_game_question.correct_answer_key
+          }}.not_to change { game_w_questions.current_level }
+      end
+
+      it { expect(response.status).not_to eq(200) }
+      it { expect(response).to redirect_to(new_user_session_path) }
+      it { expect(flash[:alert]).to be }
     end
 
-    it 'kicks from #take_money' do
-      expect { put take_money_game_path(game_w_questions) }.
-        not_to change { game_w_questions.finished_at }
+    describe '#take_money' do
+      before(:example) do
+        put take_money_game_path(game_w_questions)
+      end
 
-      expect(response.status).not_to eq(200)
-      expect(response).to redirect_to(new_user_session_path)
-      expect(flash[:alert]).to be
+      it 'does not finish game' do
+        expect { put take_money_game_path(game_w_questions) }.
+          not_to change { game_w_questions.finished_at }
+      end
+
+      it { expect(response.status).not_to eq(200) }
+      it { expect(response).to redirect_to(new_user_session_path) }
+      it { expect(flash[:alert]).to be }
     end
 
-    it 'kicks from #help' do
-      expect { put take_money_game_path(game_w_questions) }.
-        to not_change { game_w_questions.fifty_fifty_used }.
-        and not_change { game_w_questions.audience_help_used }.
-        and not_change { game_w_questions.friend_call_used }
+    describe '#help' do
+      before(:example) do
+        put help_game_path(game_w_questions)
+      end
 
-      expect(response.status).not_to eq(200)
-      expect(response).to redirect_to(new_user_session_path)
-      expect(flash[:alert]).to be
+      it 'does not change help status' do
+        expect { put help_game_path(game_w_questions) }.
+          to not_change { game_w_questions.fifty_fifty_used }.
+          and not_change { game_w_questions.audience_help_used }.
+          and not_change { game_w_questions.friend_call_used }
+      end
+
+      it { expect(response.status).not_to eq(200) }
+      it { expect(response).to redirect_to(new_user_session_path) }
+      it { expect(flash[:alert]).to be }
     end
   end
 
-  context 'Usual user' do
+  context 'Registered user' do
+    let(:admin) { create(:user, is_admin: true) }
     let(:another_game) { create(:game_with_questions) }
 
     before(:example) do
       sign_in user
     end
 
-    it 'creates game' do
-      generate_questions(60)
+    describe '#create' do
+      context 'user has no unfinished game' do
+        it 'creates game' do
+          generate_questions(60)
 
-      post games_path
+          post games_path
 
-      game = @controller.view_assigns['game']
+          game = controller.view_assigns['game']
 
-      expect(game.finished?).to be_falsey
-      expect(game.user).to eq(user)
+          expect(game.finished?).to be_falsey
+          expect(game.user).to eq(user)
 
-      expect(response).to redirect_to(game_path(game))
-      expect(flash[:notice]).to be
+          expect(response).to redirect_to(game_path(game))
+          expect(flash[:notice]).to be
+        end
+      end
+
+      context 'user has unfinished game' do
+        it 'forbids to create game before finish another' do
+          expect(game_w_questions.finished?).to be_falsey
+
+          expect { post games_path }.not_to change(Game,:count)
+
+          game = controller.view_assigns['game']
+
+          expect(game).to be_nil
+          expect(response.status).not_to eq(200)
+          expect(response).to redirect_to(game_path(game_w_questions))
+          expect(flash[:alert]).to be
+        end
+      end
     end
 
-    it 'forbids to create game before finish another' do
-      expect(game_w_questions.finished?).to be_falsey
+    describe '#show' do
+      context 'when users own game' do
+        let(:game) { controller.view_assigns['game'] }
 
-      expect { post games_path }.not_to change(Game,:count)
+        before(:example) do
+          get game_path(game_w_questions)
+        end
 
-      game = @controller.view_assigns['game']
+        it { expect(game.finished?).to be_falsey }
+        it { expect(game.user).to eq(user) }
+        it { expect(response.status).to eq(200) }
+      end
 
-      expect(game).to be_nil
-      expect(response.status).not_to eq(200)
-      expect(response).to redirect_to(game_path(game_w_questions))
-      expect(flash[:alert]).to be
+      context 'when other users game' do
+        before(:example) do
+          get game_path(another_game)
+        end
+
+        it { expect(response.status).not_to eq(200) }
+        it { expect(response).to redirect_to(root_path) }
+        it { expect(flash[:alert]).to be }
+      end
     end
 
-    it 'shows game' do
-      get game_path(game_w_questions)
+    describe '#answer' do
+      context 'when answer is correct' do
+        let(:game) { game = controller.view_assigns['game'] }
 
-      game = @controller.view_assigns['game']
+        before(:example) do
+          put answer_game_path(game_w_questions), params: {
+            letter: game_w_questions.current_game_question.correct_answer_key
+          }
+        end
 
-      expect(game.finished?).to be_falsey
-      expect(game.user).to eq(user)
+        it { expect(game.finished?).to be_falsey }
+        it { expect(game.current_level).to be > 0 }
+        it { expect(response).to redirect_to(game_path(game)) }
+        it { expect(flash.empty?).to be_truthy }
+      end
 
-      expect(response.status).to eq(200)
+      context 'when answer is incorrect' do
+        let(:game) { game = controller.view_assigns['game'] }
+
+        before(:example) do
+          put answer_game_path(game_w_questions), params: { letter: 'a' }
+        end
+
+        it { expect(game.finished?).to be_truthy }
+        it { expect(game.is_failed).to be_truthy }
+        it { expect(response).to redirect_to(user_path(user)) }
+        it { expect(flash[:alert]).to be }
+      end
     end
 
-    it 'forbids to access someone elses game' do
-      get game_path(another_game)
+    describe '#take_money' do
+      let(:game) { game = controller.view_assigns['game'] }
 
-      expect(response.status).not_to eq(200)
-      expect(response).to redirect_to(root_path)
-      expect(flash[:alert]).to be
-    end
+      before(:example) do
+        game_w_questions.update(current_level: 4)
+        put take_money_game_path(game_w_questions)
+      end
 
-    it 'answer is correct' do
-      put answer_game_path(game_w_questions),
-        params: {
-          letter: game_w_questions.current_game_question.correct_answer_key
-        }
+      it { expect(game.finished?).to be_truthy }
+      it { expect(game.prize).to eq(500) }
+      it { expect(response).to redirect_to(user_path(user)) }
+      it { expect(flash[:warning]).to be }
 
-      game = @controller.view_assigns['game']
-
-      expect(game.finished?).to be_falsey
-      expect(game.current_level).to be > 0
-      expect(response).to redirect_to(game_path(game))
-      expect(flash.empty?).to be_truthy
-    end
-
-    it 'answer is incorrect' do
-      put answer_game_path(game_w_questions), params: { letter: 'a' }
-
-      game = @controller.view_assigns['game']
-
-      expect(game.finished?).to be_truthy
-      expect(game.is_failed).to be_truthy
-      expect(response).to redirect_to(user_path(user))
-      expect(flash[:alert]).to be
-    end
-
-    it 'takes money' do
-      game_w_questions.update(current_level: 4)
-
-      put take_money_game_path(game_w_questions)
-
-      game = @controller.view_assigns['game']
-
-      expect(game.finished?).to be_truthy
-      expect(game.prize).to eq(500)
-
-      user.reload.balance
-
-      expect(user.balance).to eq(game.prize)
-
-      expect(response).to redirect_to(user_path(user))
-      expect(flash[:warning]).to be
+      it 'adds correct sum tousers balance' do
+        user.reload.balance
+        expect(user.balance).to eq(game.prize)
+      end
     end
   end
 end
